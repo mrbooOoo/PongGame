bc5378fb2ca119c336cbec2c5b9b8d655c75ef9f
Carlos Oliveira <al62898@utad.eu>
2021-06-29 17:16:01 +0100

    create ball animation function

--- setup.py
+++ setup.py
@@ -14,12 +14,32 @@ pygame.display.set_caption('Pong')
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30) #defining a rect with 30x30 centered in the screen
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
-midline = pygame.Rect(screen_width/2, 0, 2, screen_height)
+midline = pygame.Rect(screen_width/2, 0, 1, screen_height)
 
 #Definition of colors
 bg_color = pygame.Color('grey12')
 grey = (200, 200, 200)
 
+#Ball speed
+ball_speed_x = 7
+ball_speed_y = 7
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+
+    #Ball Speed
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    #Barriers for the board
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1  #inverse movement of y
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1  #inverse movement of x
+    
+    #Collisions with the bars
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
 
 while True:
     #Handling input
@@ -27,6 +47,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     #Visuals / Drawing the structures
     screen.fill(bg_color) #filling the color 'grey12' for the background (bg)
     pygame.draw.rect(screen, grey, player)
@@ -34,6 +55,8 @@ while True:
     pygame.draw.ellipse(screen, grey, ball)
     pygame.draw.rect(screen, grey, midline)
 
+    #Running ball animation function
+    ball_animation()
 
     #Updating the window 
     pygame.display.flip()
